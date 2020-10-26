ec4101fcf6b71ff6bffd8ab9154cc8638d74466d
snystrom <snystrom@users.noreply.github.com>
2020-10-25 23:04:56 -0400

    Added d4_install_cargo function

--- R/install.R
+++ R/install.R
@@ -30,3 +30,36 @@ d4_install_check <- function(d4utils = NULL) {
 d4_is_installed <- function(d4utils = NULL) {
   cmdfun::cmd_install_is_valid(d4_search_path, path = d4utils)
 }
+
+#' Build & Install d4utils Using Cargo
+#'
+#' A convenience function for building & installing d4utils from crates.io using
+#' cargo. Requires a Rust install and `gcc`.
+#'
+#' @param ... any valid cargo install flags passed as R function arguments. Omit
+#'   leading dashes, and replace dashes between words with underscores (_).
+#' @param cargo path to cargo if installed at a nonstandard location. Default: "~/.cargo/bin/cargo".
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' d4_install_cargo()
+#' }
+d4_install_cargo <- function(..., cargo = NULL){
+  # TODO: Test this
+  usethis::ui_yeah("This will build & install d4utils to your cargo directory")
+
+  if(!(cargo_is_installed(path = cargo))) {
+    stop("Cargo is not installed, or cannot be detected.")
+  }
+
+  flags <- cmdfun::cmd_args_dots() %>%
+    cmdfun::cmd_list_interp() %>%
+    cmdfun::cmd_list_to_flags()
+
+  cargo <- cargo_path_search(path = cargo)
+  processx::run(cargo, c("install", "d4utils", flags))
+
+}
